bf0f3310f66aea66e32666ea61fb47e20a4d6cee
Evoly <svetoch.sv@gmail.com>
2019-11-27 16:46:34 +0700

    menu_new

--- dist/js/index.js
+++ dist/js/index.js
@@ -321,6 +321,8 @@
 
   var headerComponent = function headerComponent() {
     if ($(window).width() > 767) {
+      var window_width = $(window).width();
+      console.log('window_width', window_width);
       $('.js-dropdown').hover(function () {
         $(this).children('[data-toggle="dropdown"]').click();
       }, function () {
@@ -328,6 +330,9 @@
       });
     }
     $(window).resize(function () {
+      var window_width = $(window).width();
+      console.log('window_width-resize', window_width);
+
       if ($(window).width() > 767) {
         $('.js-dropdown').hover(function () {
           $(this).children('[data-toggle="dropdown"]').click();
@@ -335,14 +340,16 @@
           $(this).children('[data-toggle="dropdown"]').click();
         });
       }
-    }); // $('.navbar-collapse').on('click', function() {
-    //   $(document).mouseup(function(e) {
-    //     const container = $(".navbar__nav");
-    //     if (e.target != container[0] && container.has(e.target).length === 0) {
-    //       $('.navbar-collapse').collapse('hide')
-    //     }
-    //   });
-    // });
+    });
+    $('.navbar-collapse').on('click', function () {
+      $(document).mouseup(function (e) {
+        var container = $(".navbar__nav");
+
+        if (e.target != container[0] && container.has(e.target).length === 0) {
+          $('.navbar-collapse').collapse('hide');
+        }
+      });
+    });
   };
 
   var mainPage = function mainPage() {
--- src/js/headerComponent.js
+++ src/js/headerComponent.js
@@ -1,5 +1,7 @@
 export const headerComponent = () => {
   if ($(window).width() > 767) {
+    const window_width = $(window).width();
+    console.log('window_width', window_width);
     $('.js-dropdown').hover(function(){
       $(this).children('[data-toggle="dropdown"]').click();
   }, function(){
@@ -8,6 +10,8 @@ export const headerComponent = () => {
 };
 
   $(window).resize(function() {
+    const window_width = $(window).width();
+    console.log('window_width-resize', window_width);
   if ($(window).width() > 767) {
     $('.js-dropdown').hover(function(){
       $(this).children('[data-toggle="dropdown"]').click();
@@ -17,13 +21,13 @@ export const headerComponent = () => {
   }
 });
 
-// $('.navbar-collapse').on('click', function() {
-//   $(document).mouseup(function(e) {
-//     const container = $(".navbar__nav");
-//     if (e.target != container[0] && container.has(e.target).length === 0) {
-//       $('.navbar-collapse').collapse('hide')
-//     }
-//   });
-// });
+$('.navbar-collapse').on('click', function() {
+  $(document).mouseup(function(e) {
+    const container = $(".navbar__nav");
+    if (e.target != container[0] && container.has(e.target).length === 0) {
+      $('.navbar-collapse').collapse('hide')
+    }
+  });
+});
 
 };
